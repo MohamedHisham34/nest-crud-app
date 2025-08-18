33e07f13a3e7cd570354e2269819d5e4cfa5d5c3
MohamedHisham34 <mohamedhisham342000@gmail.com>
2025-08-18 12:47:39 +0300

    Added MiddleWare Filters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestMiddleware, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from 'src/auth/auth.controller';
 import { AuthService } from 'src/auth/auth.service';
+import { LoggerMiddleware } from 'src/middleware/logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, AuthController],
-  providers: [AppService , AuthService],
+  providers: [AppService, AuthService],
 })
-export class AppModule {}
+
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('auth');
+  }
+}
